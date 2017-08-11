401b9a518c0e53383605d2a0b36c53486dcaf022
GauthierElixis <gcornette@elixis.com>
2017-08-11 17:52:08 +0200

    Update with lib mysql query

--- app.js
+++ app.js
@@ -4,8 +4,20 @@
 var md5 = require('md5');
 var express = require('express');
 var app = express();
+
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+
+var mysql   = require('mysql');
+var request   = require('request');
+
+var db = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'club',
+});
 
 app.use(express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
